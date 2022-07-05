780e5537a090731ea143e3e24a9c02d1972d4f26
sharpdlt <sharpdlt@gmail.com>
2022-07-05 13:44:14 +0300

    permission_classes

--- api/views.py
+++ api/views.py
@@ -1,11 +1,23 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
-from main.models import Course
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from main.models import Course, Group, Role, Student
 from api.serializers import CourseSerializer
 
 
-class CourseList(generics.ListCreateAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
+class CourseList(APIView):
+    permission_classes = [IsAuthenticated]
+
+    # queryset = Course.objects.all()
+    # serializer_class = CourseSerializer
+    def get(self, request):
+        user = self.request.user.id
+        student = Student.objects.get(user_id=user)
+        group = Group.objects.get(pk=student.group_id)
+        course = Course.objects.get(pk=group.course_id)
+        serializer = CourseSerializer(course)
+        return Response(serializer.data)
 
 
 class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
